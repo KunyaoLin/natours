4d625bd2c9df1b730cfa96100ae49316556c9f25
Kunyao <kunyaolin31@gmail.com>
2024-10-09 20:53:26 -0300

    updated checkout3

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -45,7 +45,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.display_items[0].amount / 100;
+  const price = session.amount_total / 100;
   await Booking.create({ tour, user, price });
 };
 exports.webhookCheckout = (req, res, next) => {
@@ -60,10 +60,10 @@ exports.webhookCheckout = (req, res, next) => {
   } catch (err) {
     return res.status(400).send(`Webhook error:${err.message}`);
   }
-  console.log('Create booking now');
-  if (event.type === 'checkout.session.completed') {
-    console.log(event);
-    // createBookingCheckout(event.data.object);
+  // console.log('Create booking now');
+  if (event.status === 'complete') {
+    // console.log(event);
+    createBookingCheckout(event.object);
   }
 
   res.status(200).json({
